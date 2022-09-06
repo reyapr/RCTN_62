429c5d99e19fb968b9dbd1e678de82dbed42211d
Renal Apriansyah <renal.apr@gmail.com>
2022-09-06 21:09:37 +0700

    sesi 3 - learn async await

--- sesi_3/learnFetch.js
+++ sesi_3/learnFetch.js
@@ -3,13 +3,36 @@ import axios from 'axios'
 axios('https://swapi.dev/api/people')
     .then(result => result.data.results.filter(data => data.height > 165))
     .then(result => {
-        console.log(result, `<=================== result ==================`);
+        // console.log(result, `<=================== result ==================`);
     })
     .catch(err => {
-        console.log(err, `<=================== err ==================`);
+        // console.log(err, `<=================== err ==================`);
     })
 
+const fetchData = async () => {
+    try {
+        const result = await axios('https://swapi.dev/api/peop2le')
+        const heightAbove165 = result.data.results
+            .filter(data => data.height > 165)
+        return heightAbove165
+    } catch(err) {
+        throw err
+    }
+    
+    
+    
+}
 
+const showData = async () => {
+   try {
+    const filteredData = await fetchData()
+    console.log(filteredData, `<=================== filteredData ==================`);
+   } catch (error) {
+    console.log(error, `<=================== err ==================`);
+   }
+}
+  
+showData()  
 
 const res = 1 + 2
 console.log(res, `<===================  ==================`);
