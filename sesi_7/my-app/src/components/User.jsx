dde05eabe5e341b34d8e83752beef1a5890f471d
Renal Apriansyah <renal.apr@gmail.com>
2022-09-21 20:53:32 +0700

    sesi 7 - updating -> component did update

--- sesi_7/my-app/src/components/User.jsx
+++ sesi_7/my-app/src/components/User.jsx
@@ -5,7 +5,23 @@ class User extends React.Component {
     constructor() {
         super()
         this.state = {
-            users: []
+            users: [],
+            userId: null,
+            userDetails: null
+        }
+    }
+    
+    getDetails(userId) {
+        this.setState({ userId })
+    }
+    
+    async componentDidUpdate(prevProps, prevState) {
+        if(prevState.userId !== this.state.userId) {
+            const url = `https://jsonplaceholder.typicode.com/users/${this.state.userId}`
+            const res = await axios.get(url)
+            this.setState({
+                userDetails: res.data
+            })
         }
     }
     
@@ -21,11 +37,26 @@ class User extends React.Component {
             <div>
                 <ul>
                     {
+                        this.state.userDetails ? 
+                        <>
+                            <div>Name: {this.state.userDetails.name}</div>
+                            <div>Email: {this.state.userDetails.email}</div>
+                            <div>phone: {this.state.userDetails.phone}</div>
+                            <button 
+                                onClick={() => this.setState({userDetails: null})}
+                            >
+                                Back
+                            </button>
+                        </>
+                        :
                         this.state.users.map(user => {
                             return (
                                 <li key={user.id} style={{marginBottom: '10px'}}>
                                     <div>Name: {user.name}</div> 
                                     <div>Email: {user.email}</div>
+                                    <button onClick={() => this.getDetails(user.id)}>
+                                        Get Details
+                                    </button>
                                 </li>
                             )
                         })
