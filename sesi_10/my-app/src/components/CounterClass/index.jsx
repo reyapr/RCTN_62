d2f40b7a0881fceb4a02148895dfe6568dffb178
Renal Apriansyah <renal.apr@gmail.com>
2022-09-29 20:18:03 +0700

    sesi 10 - react-redux action old

--- sesi_10/my-app/src/components/CounterClass/index.jsx
+++ sesi_10/my-app/src/components/CounterClass/index.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { incrementClass, decrementClass, updateCounter } from './reducer';
 
 class CounterClass extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            inputCounter: 0
+        }
+    }
+    
+    
+    handleChange = (e) => {
+        const num = Number(e.target.value)
+        this.setState({
+            inputCounter: num
+        })
+    }
+    
+    handleSubmit = () => {
+        this.props.dispatch(updateCounter(this.state.inputCounter))
+    }
+    
     render() {
         return (
             <div>
                 <h1>{this.props.myTitleClass}</h1>
                 <h2>{this.props.myCounterClass}</h2>
-                <button>+</button>
-                <button>-</button>
+                <button onClick={() => this.props.dispatch(incrementClass)}>+</button>
+                <button onClick={() => this.props.dispatch(decrementClass)}>-</button>
                 <div>
-                    <input type="text" />
-                    <button>submit</button>
+                    <input type="text" onChange={this.handleChange} />
+                    <button onClick={this.handleSubmit}>submit</button>
                 </div>
             </div>
         )
