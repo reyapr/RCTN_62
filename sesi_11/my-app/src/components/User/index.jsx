231cd0fe1f232322a33dd79de1fe30a7150561cf
Renal Apriansyah <renal.apr@gmail.com>
2022-10-04 20:10:57 +0700

    sesi 11 - react-redux async login test case

--- sesi_11/my-app/src/components/User/index.jsx
+++ sesi_11/my-app/src/components/User/index.jsx
@@ -1,10 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { decrement, fetchUsers, increment } from "./slicer"
+import { decrement, fetchUsers, increment, authLogin } from "./slicer"
 
 const User = () => {
     const users = useSelector((state) => state.users)
     const dispatch = useDispatch()
+    const [inputLoginForm, setLoginForm] = useState({
+        email: '',
+        password: ''
+    })
+    
+    const handleInputLoginForm = (e) => {
+        console.log(e.target.value)
+        console.log(e.target.name)
+        setLoginForm({
+            ...inputLoginForm,
+            [e.target.name]: e.target.value
+        })
+    }
     
     useEffect(() => {
         dispatch(fetchUsers())
@@ -18,6 +31,26 @@ const User = () => {
     
     return (
         <div>
+            <div>
+                <div>
+                    Email: <input name="email" type="text" onChange={handleInputLoginForm}/>
+                </div>
+                <div>
+                    Password: 
+                    <input 
+                        name="password" 
+                        onChange={handleInputLoginForm}
+                        type="password"
+                    />
+                </div>
+                <button onClick={() => dispatch(authLogin(inputLoginForm))}>submit</button>
+                <div>
+                    { users.login.isLoading && <>...Loading</> }
+                    { users.login.errorMessage && !users.login.isLoading && <>{users.login.errorMessage}</>}
+                    { users.login.user.email && <>Success</>}
+                </div>
+            </div>
+            <hr />
             <h1>List of User</h1>
             {
                 users.isLoading ? 
--- sesi_11/my-app/src/components/User/slicer.js
+++ sesi_11/my-app/src/components/User/slicer.js
@@ -1,17 +1,43 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const USERS_URL = 'https://jsonplaceholder.typiasdadscode.com/users'
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
 const initialState = {
     data: [],
     count: 0,
     isLoading: false,
-    isError: false
+    isError: false,
+    login: {
+        user: {},
+        errorMessage: '',
+        isLoading: false
+    }
+}
+
+const callLoginApi = (email, password) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(email === 'admin@login.com' && password === 'admin') {
+                resolve({email})
+            } else {
+                reject('Invalid Email or Password')
+            }
+        }, 2000);
+    })
 }
 
 export const fetchUsers = createAsyncThunk('user/fetch', async () => {
     const response = await axios.get(USERS_URL)
     return response.data
+})
+
+export const authLogin = createAsyncThunk('user/login', async ({email, password}) => {
+    try {
+        const response = await callLoginApi(email, password)
+        return response.email
+    } catch (error) {
+        throw(error)
+    }    
 })
 
 const userReducer = createSlice({
@@ -26,6 +52,7 @@ const userReducer = createSlice({
         }
     },
     extraReducers: (builder) => {
+        // Fetch Users
         builder.addCase(fetchUsers.fulfilled, (state, action) => {
             state.data = action.payload
             state.count = action.payload.length
@@ -37,6 +64,21 @@ const userReducer = createSlice({
         builder.addCase(fetchUsers.rejected, (state) => {
             state.isLoading = false
             state.isError = true
+        })
+        // Login
+        builder.addCase(authLogin.pending, (state) => {
+            state.login.isLoading = true
+            state.login.errorMessage = ''
+            state.login.user = {}
+        })
+        builder.addCase(authLogin.fulfilled, (state, action) => {
+            state.login.isLoading = false
+            state.login.user.email = action.payload
+            state.login.errorMessage = ''
+        })
+        builder.addCase(authLogin.rejected, (state, action) => {
+            state.login.isLoading = false
+            state.login.errorMessage = action.error.message
         })
     }
 })
